9ab98f672fc8a6e8e8928e55d7b5a086d43dcbe7
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2022-09-24 19:11:27 +0200

    add users fixture + test grid model

--- test/models/grid_test.rb
+++ test/models/grid_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class GridTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @opponent = users(:two)
+  end
+  
+  test "should belong an user and an opponent" do
+    grid = Grid.new(user: @user, opponent: @opponent)
+    assert_equal @opponent, grid.opponent
+    assert_equal @user, grid.user
+  end
+
+  test "should be in progress by default" do
+    grid = Grid.new(user: @user, opponent: @opponent)
+    assert grid.in_progress?
+  end
 end
