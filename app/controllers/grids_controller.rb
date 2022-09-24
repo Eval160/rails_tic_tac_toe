3c3ccfcfc48672916f19f916817422b79bd9fe17
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2022-09-24 21:35:01 +0200

    new and create grid

---
+++ app/controllers/grids_controller.rb
@@ -0,0 +1,34 @@
+class GridsController < ApplicationController
+  before_action :set_grid, only: [:show]
+
+  def new
+    @grid = Grid.new
+  end
+
+  def create
+    @grid = Grid.new(grid_params)
+    @grid.user = current_user
+    if @grid.save
+      redirect_to grid_path(@grid)
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+  
+  
+  private
+
+  def grid_params
+    params.require(:grid).permit(:opponent_id)
+  end
+
+  def set_grid
+    @grid = Grid.find(params[:id])
+  end
+  
+  
+end
