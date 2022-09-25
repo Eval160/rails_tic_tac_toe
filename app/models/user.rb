1b361b0fff4a4343757fec762b4e329c8d232ce3
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2022-09-25 19:18:59 +0200

    add faker + add users to seed + helper method ia

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :owned_grids, class_name: "Grid", foreign_key: "user_id", dependent: :destroy
   has_many :involved_grids, class_name: "Grid", foreign_key: "opponent_id", dependent: :destroy
 
   def grids
     Grid.where("user_id = :user OR opponent_id = :user", user: self.id)
+  end
+
+  def self.ia
+    User.find_by(email: "ia@mail.com")
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
 
 def clean_db
-  User.delete_all
+  User.destroy_all
 end
 
 def create_users
+  User.create!(email: "ia@mail.com", password: "azerty", nickname: "Ordinateur")
   User.create!(email: "riri@mail.com", password: "azerty", nickname: "Riri")
   User.create!(email: "fifi@mail.com", password: "azerty", nickname: "Fifi")
   User.create!(email: "loulou@mail.com", password: "azerty", nickname: "Loulou")
+
+  15.times do |n|
+    User.create!(email: "user_#{n}@mail.com", password: "azerty", nickname: Faker::Games::Pokemon.name )
+  end
 end
 
 
 puts "Clean DB"
 clean_db
+puts "DB cleaned"
+
+puts "Create users"
 create_users
+puts "#{User.count} users created"
+
