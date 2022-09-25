99cd28690326799abeabdac2717e04962360efbd
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2022-09-25 15:38:58 +0200

    compute cell's coordonates x and y + test

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -3,4 +3,30 @@ class Cell < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :position, uniqueness: { scope: :grid }
+
+  after_update :user_won?
+
+  def position_x
+    (self.position / Grid::SIZE) + 1
+  end
+
+  def position_y
+    return 1 if self.position.zero?
+    (self.position % Grid::SIZE + 1)
+  end
+  
+  
+  private
+  
+  def user_won?
+    return if less_than_3_turns_by_player
+    
+  end
+
+  def less_than_3_turns_by_player
+    self.grid.players.all? do |player|
+      self.grid.cells.where(user: player).count < 3
+    end
+  end
+  
 end
--- test/models/cell_test.rb
+++ test/models/cell_test.rb
@@ -9,4 +9,17 @@ class CellTest < ActiveSupport::TestCase
     assert cell_to_update.errors[:position].present?, "should have an error to position attribute"
     assert cell_to_update.errors.details[:position].any?{|e| e[:error] == :taken}, "should have taken error"
   end
+
+  test "should return x and y coordonates" do
+    grid = Grid.create!(user: User.first, opponent: User.last)
+
+    grid.cells.each_slice(Grid::SIZE).with_index do |row, x|
+      position_x = x + 1
+      row.each_with_index do |cell, y|
+        position_y = y + 1
+        assert_equal position_x, cell.position_x
+        assert_equal position_y, cell.position_y
+      end
+    end
+  end
 end
